a3a8fb90169a63eee4fcea5665b6c695215c69bf
Nicolai Huener <[nicolai.huener@hpe.com]>
2018-10-13 16:36:48 +0200

    all users finally working

--- frontend/static/html/compare.js
+++ frontend/static/html/compare.js
@@ -1,14 +1,23 @@
-function getPictures(){
-     var response;
-     var pictureNumber;
-     var xmlhttp = new XMLHttpRequest();
-     xmlhttp.open("GET", "http://localhost:8080/meetme/api/shootout")
-     response = xmlhttp.responseText;
-     pictureNumber = response.length;
-     alert(response);
+window.onload = function () {
+    var response;
+    var pictureNumber;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "http://localhost:8080/meetme/api/shootout")
+    xmlhttp.send(null);
+    response = xmlhttp.responseText;
+    var shootout = JSON.parse(response);
+    alert(shootout);
+};
+//window.onload(getPictures());
+   /* var xmlhttpr = new XMLHttpRequest();
+    xmlhttpr.open("GET", "http://localhost:8080/meetme/api/photo")
+    var response2 = xmlhttp.responseText;
+    var photo = JSON.parse(response2);
+pictureNumber = (Object.keys(photo).length);
 
+var pic1 = photo[1].picture;
      for (var i = 0; i < pictureNumber; i++){
-     var pic1 = JSONGetElement(response;);
+
      }
 
 }
@@ -18,5 +27,4 @@ function comparePicture(){
 }
 
 function comparePicture2(){
-    var xmlhttp = new XMLHttpRequest();
-}
+    var xmlhttp = new XMLHttpRequest();*/
