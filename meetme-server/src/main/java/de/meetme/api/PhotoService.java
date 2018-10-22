d7a8f1f06a80c996bd750262112d26882ab87104
Nicolai Huener <[nicolai.huener@hpe.com]>
2018-10-22 11:56:46 +0200

    shootout rank html mostly working

--- meetme-server/src/main/java/de/meetme/api/PhotoService.java
+++ meetme-server/src/main/java/de/meetme/api/PhotoService.java
@@ -3,16 +3,20 @@ package de.meetme.api;
 import de.meetme.data.Photo;
 import de.meetme.db.PhotoDao;
 import io.dropwizard.hibernate.UnitOfWork;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import  java.io.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @Path("/photo") // Part of the URL to identify this resource
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+
     public class PhotoService {
 
     private static final Logger log = LoggerFactory.getLogger(de.meetme.api.PhotoService.class);
@@ -24,14 +28,19 @@ import java.util.List;
 
     @PUT
     @Path("/wins/{photoid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @UnitOfWork
     public void updatewins( @PathParam("photoid")long photoid) throws Exception{
         log.debug("Update Wins: " + photoid);
         dao.updatewins(photoid);
     }
 
+
     @PUT
     @Path("/clicks/{clicks}&{photoid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @UnitOfWork
     public void updateclicks(@PathParam("clicks")int clicks,@PathParam("photoid")long photoid){
         log.debug("Update clicks: " + photoid);
@@ -39,6 +48,8 @@ import java.util.List;
     }
 
     @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @UnitOfWork  //  be transaction aware (This tag automatically creates a database transaction with begin/commit or rollback in case of an error
     public void updatePhoto(Photo photo) throws Exception {
         log.debug("Update Photo: " + photo);
@@ -46,6 +57,8 @@ import java.util.List;
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("/{id}")
     @UnitOfWork
     //  be transaction aware (This tag automatically creates a database transaction with begin/commit or rollback in case of an error
@@ -56,8 +69,58 @@ import java.util.List;
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @UnitOfWork  //  be transaction aware (This tag automatically creates a database transaction with begin/commit or rollback in case of an error
     public List<Photo> getPhotos() {
         return  dao.getAll();
     }
+
+   /* @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+        OutputStream out = null;
+        String fileName = fileDetail.getFileName();
+        String filePath = "./src/main/uploads/" + fileName;
+        //LocalDate createdDate = java.time.LocalDate.now();
+        // save it
+        //writeToFile(uploadedInputStream, uploadedFileLocation);
+
+        // save uploaded file to new location
+        //writeToFile(uploadedInputStream, uploadedFileLocation) {
+
+        try {
+            //OutputStream out = null;
+            File fileToUpload = new File(filePath);
+            out = new FileOutputStream(fileToUpload);
+            int read;
+            byte[] bytes = new byte[2048];
+
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Response.status(500).build();
+        } finally {
+            try {
+            out.close();
+        }catch (IOException e){
+            e.printStackTrace();
+           return Response.status(500).build();
+        }
+
+
+
+    }
+
+        return Response.status(200).build();
+
+}*/
 }
+
